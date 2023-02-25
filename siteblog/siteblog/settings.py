95a022b4448659013144bc7b2a6afe12839e7245
vitalii4709 <42183340+vitalii4709@users.noreply.github.com>
2023-02-25 16:15:27 +0300

    Update settings.py

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-qlp#(&gl=a%-zg)8vu+r^&o5u)w^lb*a7(la2f5cfc2nkca6b&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
